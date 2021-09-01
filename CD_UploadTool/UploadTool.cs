3bc3fcb28fc809d0c232616dc64b8c83b4f74bd8
Rene.Viehhauser <Rene.Viehhauser@copadata.com>
2021-09-01 08:41:11 +0200

    Add project files.

---
+++ CD_UploadTool/UploadTool.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Security;
+using System.Text;
+using System.Xml;
+using Limilabs.FTP.Client;
+
+namespace CD_UploadTool
+{
+    class UploadTool
+    {
+        //bsp: t=“renew“ pw=“3ra96erznG/e“ un=“203390-releases-intern“ host=“ftp.copadata.at“ folder=“/Dokumentation/1000SP0“
+        public static void renew(string username, string password, string host, string folder)
+        {
+            string local_save = "C:\\ProgramData\\LANGUAGES.xml";
+            using (Ftp client = new Ftp())
+            {
+                // Use this line to validate self-signed certificates:
+                client.ServerCertificateValidate += ValidateCertificate;
+
+                client.Connect(host);
+                client.AuthTLS();
+                client.Login(username, password);
+
+                WriteXml(client, folder, local_save);
+                UploadTrueFilename(client,folder,local_save);
+                File.Delete(local_save);
+
+                Console.WriteLine("Renewing of xml successful!");
+                client.Close();
+            }
+        }
+
+        //bsp: t=“upload“ pw=“3ra96erznG/e“ un=“203390-releases-intern“ host=“ftp.copadata.at“ path=“/Dokumentation/1000SP0/Test_upload“ src=“G:\zenon1000\FINAL\HELP\Supervisor\en.cdhelp“
+        public static void upload(string username, string password, string host, string path, string src)
+        {
+            string[] file = src.Split('/', '\\');
+            string filepath = path + "/" + file[file.Length - 1];
+            using (Ftp client = new Ftp())
+            {
+                // Use this line to validate self-signed certificates:
+                client.ServerCertificateValidate += ValidateCertificate;
+
+                client.Connect(host);
+                client.AuthTLS();
+                client.Login(username, password);
+
+                UploadTrueFilename(client, path, src);
+
+                Console.WriteLine("Uploading of file successful!");
+                client.Close();
+            }
+        }
+
+        private static void WriteXml(Ftp client, string folder, string save)
+        {
+            XmlWriterSettings oSettings = new XmlWriterSettings();
+            oSettings.Indent = true;
+            oSettings.OmitXmlDeclaration = false;
+            oSettings.Encoding = Encoding.UTF8;
+            using (XmlWriter writer = XmlWriter.Create(save, oSettings))
+            {
+                writer.WriteStartDocument(false);
+                writer.WriteStartElement("Languages");
+                GetSubelements(client, folder, writer);
+                writer.WriteEndDocument();
+                writer.Flush();
+            }
+        }
+
+        private static void GetSubelements(Ftp client, string folder, XmlWriter writer)
+        {
+            client.ChangeFolder(folder);
+
+            foreach (FtpItem item in client.GetList())
+            {
+                if (item.IsFolder == true)
+                {
+                    if (HasFile(client, item.Name))
+                    {
+                        if(GetGroup(item.Name) != "")
+                        {
+                            writer.WriteStartElement(GetGroup(item.Name));
+                            writer.WriteStartAttribute("dir");
+                            writer.WriteValue(item.Name);
+                            writer.WriteEndAttribute();
+                        }else
+                        {
+                            writer.WriteStartElement(item.Name);
+                        }
+                        GetSubelements(client, item.Name,writer);
+                    }  
+                }
+                else if((item.Name != ".")&&(item.Name != "..")&&(item.Name != "LANGUAGES.xml"))
+                {
+                    writer.WriteStartElement("file");
+                    writer.WriteStartAttribute("name");
+                    writer.WriteValue(item.Name);
+                    writer.WriteEndAttribute();
+                    writer.WriteStartAttribute("size");
+                    writer.WriteValue(item.Size);
+                    writer.WriteEndAttribute();
+                    writer.WriteStartAttribute("date");
+                    writer.WriteValue(WithoutTimeZone(item.ModifyDate));
+                    writer.WriteEndAttribute();
+                    writer.WriteEndElement();
+                }
+            }
+            writer.WriteEndElement();
+            client.ChangeFolderUp();
+        }
+
+        private static bool HasFile(Ftp client, string folder)
+        {
+            List<string> subfolders = new List<string>();
+            client.ChangeFolder(folder);
+            foreach (FtpItem item in client.GetList())
+            {
+                if (item.IsFolder)
+                {
+                    subfolders.Add(item.Name);
+                }
+                else if ((item.Name != ".") && (item.Name != "..") && (item.Name != "LANGUAGES.xml"))
+                {
+                    client.ChangeFolderUp();
+                    return true;
+                }
+            }
+            for (int i = 0; i < subfolders.Count; i++)
+            {
+                if (HasFile(client, subfolders[i]))
+                {
+                    client.ChangeFolderUp();
+                    return true;
+                }
+            }
+            client.ChangeFolderUp();
+            return false;
+        }
+
+        private static void ValidateCertificate(object sender,ServerCertificateValidateEventArgs e)
+        {
+            const SslPolicyErrors ignoredErrors =
+                SslPolicyErrors.RemoteCertificateChainErrors |
+                SslPolicyErrors.RemoteCertificateNameMismatch;
+
+            if ((e.SslPolicyErrors & ~ignoredErrors) == SslPolicyErrors.None)
+            {
+                e.IsValid = true;
+                return;
+            }
+            e.IsValid = false;
+        }
+
+        private static string GetGroup(string folder)
+        {
+            folder = folder.ToLower();
+            string group = "";
+
+            if (folder.Contains("zippedhelp"))
+            {
+                group = "zippedHelp";
+            }else if ((folder.Contains("revi"))||(folder.Contains("release")))
+            {
+                group = "ReleaseNotes";
+            }else if (folder.Contains("onlinehelp"))
+            {
+                group = "OnlineHelp";
+            }else if ((folder.Contains("manu")) || (folder.Contains("handbuch")))
+            {
+                group = "Manual";
+            }else if ((folder.Contains("driver")) || (folder.Contains("treiber")))
+            {
+                group = "Driver";
+            }else if (folder.Contains("tutorials"))
+            {
+                group = "Tutorials";
+            }
+
+            return group;
+        }
+
+        private static void UploadTrueFilename(Ftp client, string path, string source)
+        {
+            string[] file = source.Split('/', '\\');
+            string filepath = path + "/" + file[file.Length - 1];
+            string wrong_name = path + "/FtpTrial-" + file[file.Length - 1];
+            client.Upload(filepath, source);
+            if(client.FileExists(filepath) == false)
+            {
+                client.Rename(wrong_name, filepath);
+            }
+        }
+
+        public static DateTime WithoutTimeZone(DateTime dateTime)
+        {
+            if (dateTime.Kind != DateTimeKind.Unspecified)
+                return new DateTime(dateTime.Ticks, DateTimeKind.Unspecified);
+
+            return dateTime;
+        }
+    }
+}
---
+++ CD_UploadTool/UserInterface_UploadTool.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace CD_UploadTool
+{
+    class UserInterface_UploadTool
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Console.WriteLine("CD_UploadTool\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                try
+                {
+                    string input = "";
+                    string type = "";
+                    string username = "";
+                    string password = "";
+                    string host = "";
+                    string folder = "";
+                    string path = "";
+                    string source = "";
+                    Console.Write("Uploadtool>");
+                    input = Console.ReadLine();
+                    if ((input == "/h")||(input == "/H")||(input == "/help")||(input == "/Help")||(input == "/?"))
+                    {
+                        //Console.WriteLine("Here could your help text be shown! ...For the right Price of course ;) ");
+                        Console.WriteLine("Verwenden Sie /h oder /H oder /help oder /Help oder /? um diese Hilfe aufzurufen.");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Verwenden Sie Leave oder leave um zu beenden.");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Verwenden Sie t=“renew“ sowie ihr passwort als pw=“secure_passwort“ , ihren Benutzernamen als un=“Benutzer-name-secure“ , den ftp-host als host=“ftp host“ , den Ordner auf dem ftp server auf dem die xml datei erstellt bzw erneuert werden soll als folder=“/folder/ist/ordner“ um eine xml datei zu erneuern. Alle eingaben in beliebiger Reihenfolge in einer Schlange und durch Leerzeichen getrennt.");
+                        Console.WriteLine("---");
+                        Console.WriteLine("Beispielsweise:   t=“renew“ pw=“secure_passwort“ un=“Benutzer-name-secure“ host=“ftp host“ folder=“/folder/ist/ordner“");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Verwenden Sie t=“upload“ sowie ihr passwort als pw=“secure_passwort“ , ihren Benutzernamen als un=“Benutzer-name-secure“ , den ftp-host als host=“ftp host“ , den Ordner auf dem ftp server auf dem die datei hochgeladen bzw ersetzt werden soll als path=“/folder/ist/ordner“ , den Pfad auf dem die Datei auf dem Computer lokal zu finden ist als src=“C:/folder/ist/ordner/example.datei“ um eine datei hochzuladen. Alle eingaben in beliebiger Reihenfolge in einer Schlange und durch Leerzeichen getrennt.");
+                        Console.WriteLine("---");
+                        Console.WriteLine("Beispielsweise:   t=“upload“ pw=“secure_passwort“ un=“Benutzer-name-secure“ host=“ftp host“ path=“/folder/ist/ordner“ src=“C:/folder/ist/ordner/example.datei“");
+                        Console.WriteLine(" ");
+                    }
+                    else if((input == "Leave")||(input == "leave"))
+                    {
+                        endApp = true;
+                    }
+                    else
+                    {
+                        String[] inputs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                        for(int i = 0; i < inputs.Length; i++)
+                        {
+                            switch (inputs[i].ToLower().Split("=",StringSplitOptions.RemoveEmptyEntries)[0])
+                            {
+                                case "t":
+                                    if (type != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        type = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(3);
+                                        break;
+                                    }
+
+                                case "un":
+                                    if(username != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        username = inputs[i].Remove(inputs[i].Length-1, 1).Substring(4);
+                                        break;
+                                    }
+                                
+                                case "pw":
+                                    if (password != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        password = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(4);
+                                        break;
+                                    }
+
+                                case "host":
+                                    if (host != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        host = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(6);
+                                        break;
+                                    }
+
+                                case "folder":
+                                    if (folder != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        folder = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(8);
+                                        break;
+                                    }
+
+                                case "path":
+                                    if (path != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        path = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(6);
+                                        break;
+                                    }
+
+                                case "src":
+                                    if (source != "")
+                                    {
+                                        throw new Exception();
+                                    }
+                                    else
+                                    {
+                                        source = inputs[i].Remove(inputs[i].Length - 1, 1).Substring(5);
+                                        break;
+                                    }
+                            }
+                        }
+
+                        if(type == "renew")
+                        {
+                            if((password == "") ||(username == "") ||(host == "") ||(folder == ""))
+                            {
+                                throw new Exception();
+                            }
+                            else
+                            {
+                                UploadTool.renew(username,password,host,folder);
+                            }
+                        }else if(type == "upload")
+                        {
+                            if ((password == "") ||(username == "") ||(host == "") ||(path == "") ||(source == ""))
+                            {
+                                throw new Exception();
+                            }
+                            else
+                            {
+                                UploadTool.upload(username,password,host,path,source);
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                    }
+                }
+                catch (Exception g)
+                {
+                    Console.WriteLine("Error! Haben Sie vielleicht eine falsche Eingabe getätigt?");
+                    Console.WriteLine("Bitte überprüfen Sie ihre Eingabe und/oder verwenden Sie /? für Hilfe bezüglich der Parameter.");
+                }
+            }
+            return;
+        }
+    }
+}
